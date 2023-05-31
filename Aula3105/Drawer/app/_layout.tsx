28ca9fa18efeb2bd92af62714d6f88274d28c944
Leonardo João Fleith <100938180+LeonardoKako@users.noreply.github.com>
2023-05-31 12:43:23 -0300

    Add files via upload

---
+++ Aula3105/Drawer/app/_layout.tsx
@@ -0,0 +1,30 @@
+import { Drawer } from "expo-router/drawer";
+
+export default function AppLayout() {
+  return (
+    <Drawer>
+      <Drawer.Screen
+        name="index" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "Home",
+          title: "Principal",
+        }}
+      />
+      <Drawer.Screen
+        name="telaA" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "Tela A",
+          title: "Bem-vindo a Tela A",
+        }}
+      />
+      <Drawer.Screen
+        name="telaB" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "Tela B",
+          title: "Bem-vindo a Tela B",
+        }}
+      />
+    </Drawer>
+  );
+
+}
